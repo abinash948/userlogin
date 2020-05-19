5393381c2ef4d248aea48a84e4e3388b9058ad45
abinash948 <ab948.abinash@gmail.com>
2020-05-19 22:03:56 +0530

    yes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { SignInComponent } from './components/user/sign-in/sign-in.component';
-import { SignUpComponent } from './components/user/sign-up/sign-up.component';
-import { ComponentsComponent } from './components/components.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    SignInComponent,
-    SignUpComponent,
-    ComponentsComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+
+    ],
+  providers: [
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
