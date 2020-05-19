59e7d6a0d29a5a97b740e9efe8355c1bf32646eb
abinash948 <ab948.abinash@gmail.com>
2020-05-19 19:18:32 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { SignInComponent } from './components/user/sign-in/sign-in.component';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
+import { ComponentsComponent } from './components/components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
+    ComponentsComponent
   ],
   imports: [
     BrowserModule
